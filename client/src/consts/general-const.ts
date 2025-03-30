1b0940ce8a6050c8c4b1b327bd9e713640de563d
Muhammad Mavia <Muhammadmavia172@gmail.com>
2025-03-30 16:40:38 +0500

    fix: update server url

--- client/src/consts/general-const.ts
+++ client/src/consts/general-const.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://feed-app-dfdef7c63dd7.herokuapp.com";
-export const WEB_SOCKET_URL = "wss://feed-app-dfdef7c63dd7.herokuapp.com";
+export const BASE_URL = "https://feedapp-ec9fbb827489.herokuapp.com";
+export const WEB_SOCKET_URL = "wss://feedapp-ec9fbb827489.herokuapp.com";
